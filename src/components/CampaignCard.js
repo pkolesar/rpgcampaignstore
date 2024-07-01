d3a2854a8c84caea4ce4a55de6a4cf082d9ac837
Paul Kolesar <coder.kolesar@gmail.com>
2024-07-01 19:42:49 -0400

    Adding Campaign Card

---
+++ src/components/CampaignCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function CampaignCard() {
+    return (
+      <div class="CampaignCard" >
+        <header class="campaignCardTitle">Campaign Name</header>
+        
+      </div>
+    );
+  }
+  
+  export default CampaignCard;
---
+++ src/components/CampaignCardContainer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import CampaignCard from './components/CampaignCard';
+
+function CampaignCardContainer() {
+    return (
+      <div className="CampaignCardContainer">
+        <CampaignCard></CampaignCard>
+        <CampaignCard></CampaignCard>
+        <CampaignCard></CampaignCard>
+        <CampaignCard></CampaignCard>
+        <CampaignCard></CampaignCard>
+        <CampaignCard></CampaignCard>
+        <CampaignCard></CampaignCard>
+      </div>
+    );
+  }
+  
+  export default CampaignCardContainer;
